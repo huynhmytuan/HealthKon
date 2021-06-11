d50be51e49b56a0893105d96fe36a6d5e76c0cbf
Tuan Huynh <tuanhuynh@Tuans-MacBook-Pro.local>
2021-06-11 16:53:56 +0700

    Initial commit - Tuan

---
+++ app/src/main/java/com/pinkteam/android/healthkon/Controllers/MainActivity.java
@@ -0,0 +1,17 @@
+package com.pinkteam.android.healthkon.Controllers;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.pinkteam.android.healthkon.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
