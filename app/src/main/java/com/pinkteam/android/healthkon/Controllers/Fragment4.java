d9a07beb5aa0c3b73e7160b65963cf9a71c6a957
leduong307 <leduong30072000@gmail.com>
2021-06-20 16:56:58 +0700

    --Duong commit --- Ui frag

---
+++ app/src/main/java/com/pinkteam/android/healthkon/Controllers/Fragment4.java
@@ -0,0 +1,27 @@
+package com.pinkteam.android.healthkon.Controllers;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.pinkteam.android.healthkon.R;
+
+public class Fragment4 extends Fragment {
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_4, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/pinkteam/android/healthkon/Controllers/ViewPagerAdapter.java
@@ -0,0 +1,38 @@
+package com.pinkteam.android.healthkon.Controllers;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+    public ViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new Fragment1();
+            case 1:
+                return new Fragment2();
+            case 2:
+                return new Fragment3();
+            case 3:
+                return new Fragment4();
+            case 4:
+                return new Fragment5();
+            default:
+                return new Fragment1();
+
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return 5;
+    }
+}
