2d2550e681d14abaeadc42f2e446efb5a6ea99f3
leduong307 <leduong30072000@gmail.com>
2021-06-20 16:54:48 +0700

    --Duong commit --- Ui frag

--- app/src/main/java/com/pinkteam/android/healthkon/Controllers/MainActivity.java
+++ app/src/main/java/com/pinkteam/android/healthkon/Controllers/MainActivity.java
@@ -2,16 +2,92 @@ package com.pinkteam.android.healthkon.Controllers;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.pinkteam.android.healthkon.R;
 
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView mNavigationView;
+    private ViewPager mViewpager;
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mNavigationView =findViewById(R.id.bottom_nav);
+        mViewpager=findViewById(R.id.view_pager);
+        mNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.action_1:
+                        mViewpager.setCurrentItem(0);
+                        break;
+                    case R.id.action_2:
+                        mViewpager.setCurrentItem(1);
+                        break;
+                    case R.id.action_3:
+                        mViewpager.setCurrentItem(2);
+                        break;
+                    case R.id.action_4:
+                        mViewpager.setCurrentItem(3);
+                        break;
+                    case R.id.action_5:
+                        mViewpager.setCurrentItem(4);
+                        break;
+
+                }
+                return true;
+            }
+        });
+
+
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        mViewpager.setAdapter(viewPagerAdapter);
+        mViewpager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                switch (position) {
+                    case 0:
+                        mNavigationView.getMenu().findItem(R.id.action_1).setChecked(true);
+                        break;
+                    case 1:
+                        mNavigationView.getMenu().findItem(R.id.action_2).setChecked(true);
+                        break;
+                    case 2:
+                        mNavigationView.getMenu().findItem(R.id.action_3).setChecked(true);
+                        break;
+                    case 3:
+                        mNavigationView.getMenu().findItem(R.id.action_4).setChecked(true);
+                        break;
+                    case 4:
+                        mNavigationView.getMenu().findItem(R.id.action_5).setChecked(true);
+                        break;
+
+                }
+            }
+
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 }
+
