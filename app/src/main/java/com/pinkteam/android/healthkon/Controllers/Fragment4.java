d35cfbce5e7d31d2dd2b5bd0cb475baf10d4e6f0
hunhmytuan <huynhmytuan@gmail.com>
2021-07-15 15:23:21 +0700

    Tuan Commit -- Up alot activity_main

--- app/src/main/java/com/pinkteam/android/healthkon/Controllers/Fragment4.java
+++ app/src/main/java/com/pinkteam/android/healthkon/Controllers/Fragment4.java
@@ -1,10 +1,12 @@
 package com.pinkteam.android.healthkon.Controllers;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -22,6 +24,17 @@ public class Fragment4 extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_4, container, false);
+        Button button = view.findViewById(R.id.view_weight_btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(), RunRecordDetailActivity.class);
+                Bundle b = new Bundle();
+                b.putInt("journeyID", 3);
+                i.putExtras(b);
+                startActivity(i);
+            }
+        });
         return view;
     }
 }
--- app/src/main/java/com/pinkteam/android/healthkon/Controllers/RunRecordDetailActivity.java
+++ app/src/main/java/com/pinkteam/android/healthkon/Controllers/RunRecordDetailActivity.java
@@ -2,6 +2,7 @@ package com.pinkteam.android.healthkon.Controllers;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -19,20 +20,37 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.pinkteam.android.healthkon.Models.Journey;
+import com.pinkteam.android.healthkon.Models.Location;
 import com.pinkteam.android.healthkon.R;
 import com.pinkteam.android.healthkon.database.HeightUtil;
 import com.pinkteam.android.healthkon.database.JourneyUtil;
+import com.pinkteam.android.healthkon.database.LocationUtil;
+import com.pinkteam.android.healthkon.database.UserUtil;
 import com.pinkteam.android.healthkon.database.WeightUtil;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import org.jetbrains.annotations.NotNull;
 
-public class RunRecordDetailFragment extends Fragment {
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class RunRecordDetailActivity extends AppCompatActivity implements OnMapReadyCallback {
     JourneyUtil mJourneyUtil;
+    LocationUtil mLocationUtil;
     private  SimpleDateFormat dateFormat = new SimpleDateFormat("E, hh:mm dd/MM/yyyy");
+    GoogleMap mMap;
 
     Button backButton;
     Button editButton;
@@ -45,46 +63,51 @@ public class RunRecordDetailFragment extends Fragment {
 
     Journey mJourney;
 
-
-    private int journeyID = 2;
-    @Override
-    public void onAttach(@NonNull Context context) {
-        super.onAttach(context);
-    }
+    private int journeyID;
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.run_record_detail_layout, container, false);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.run_record_detail_layout);
 
-        mJourneyUtil = new JourneyUtil(getContext());
+        mJourneyUtil = new JourneyUtil(getBaseContext());
+        mLocationUtil = new LocationUtil(getBaseContext());
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        Bundle bundle = getIntent().getExtras();
+        journeyID = bundle.getInt("journeyID");
+
         mJourney = mJourneyUtil.getJourneyByID(journeyID);
 
-        dateTextview = (TextView) view.findViewById(R.id.date_textview);
-        journeyNameTextview = (TextView) view.findViewById(R.id.journey_name_textview);
-        commentTextview = (TextView) view.findViewById(R.id.comment_textview);
-        distanceTextview = (TextView) view.findViewById(R.id.distance_textview);
-        timeTextview = (TextView) view.findViewById(R.id.time_textview);
-        ratingBar = (RatingBar) view.findViewById(R.id.ratingBar);
-        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
-            @Override
-            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
-                Toast.makeText(getContext(),"Vote Rating:"+rating,Toast.LENGTH_SHORT).show();
-                mJourney.setmRating(rating);
-                mJourneyUtil.update(mJourney);
-                loadLayout();
-            }
-        });
-        loadLayout();
-    editButton = (Button) view.findViewById(R.id.edit_btn);
+        dateTextview = (TextView) findViewById(R.id.date_textview);
+        journeyNameTextview = (TextView) findViewById(R.id.journey_name_textview);
+        commentTextview = (TextView) findViewById(R.id.comment_textview);
+        distanceTextview = (TextView) findViewById(R.id.distance_textview);
+        timeTextview = (TextView) findViewById(R.id.time_textview);
+        ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+        editButton = (Button) findViewById(R.id.edit_btn);
+
         editButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 AddDialogueShow();
             }
         });
-        return view;
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                Toast.makeText(getBaseContext(),"Vote Rating:"+rating,Toast.LENGTH_SHORT).show();
+                mJourney.setmRating(rating);
+                mJourneyUtil.update(mJourney);
+                loadLayout();
+            }
+        });
+        loadLayout();
     }
+
     private void loadLayout(){
         mJourney = mJourneyUtil.getJourneyByID(journeyID);
         //get data by journey Id
@@ -110,9 +133,9 @@ public class RunRecordDetailFragment extends Fragment {
         EditText comment_Edittext;
         Button cancel_Button;
         Button save_Button;
-        LayoutInflater i = getActivity().getLayoutInflater();
+        LayoutInflater i = this.getLayoutInflater();
         View v = i.inflate(R.layout.run_record_edit_dialog, null);
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
         journey_name_Edittext = (EditText) v.findViewById(R.id.journey_name_edt);
         comment_Edittext = (EditText) v.findViewById(R.id.comment_edt);
@@ -153,5 +176,35 @@ public class RunRecordDetailFragment extends Fragment {
         //show dialog
         dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         dialog.show();
+    }
+
+    @Override
+    public void onMapReady(@NonNull @NotNull GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // draw polyline
+
+        List<Location> locations = mLocationUtil.getLocationsByJourney(journeyID);
+
+        PolylineOptions line = new PolylineOptions().clickable(false);
+        LatLng firstLoc = null;
+        LatLng lastLoc = null;
+        if(!locations.isEmpty()){
+            for(int i = 1; i < locations.size()-1; i++){
+                LatLng loc = new LatLng(locations.get(i).getmLatitude(), locations.get(i).getmLongitude());
+                line.add(loc);
+            }
+            firstLoc = new LatLng(locations.get(0).getmLatitude(), locations.get(0).getmLongitude());
+            lastLoc = new LatLng(locations.get(locations.size()-1).getmLatitude(), locations.get(locations.size()-1).getmLongitude());
+        }
+
+        float zoom = 15.0f;
+        if(lastLoc != null && firstLoc != null) {
+            mMap.addMarker(new MarkerOptions().position(firstLoc).title("Start"));
+            mMap.addMarker(new MarkerOptions().position(lastLoc).title("End"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(firstLoc, zoom));
+        }
+        mMap.addPolyline(line);
+
     }
 }
