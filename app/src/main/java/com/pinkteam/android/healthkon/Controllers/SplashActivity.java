c6013aef27be0551639effc8e8104c7d9de46118
tuanhuynh <huynhmytuan@gmail.com>
2021-07-17 01:07:38 +0700

    Tuan Commit -- update ui

--- app/src/main/java/com/pinkteam/android/healthkon/Controllers/SplashActivity.java
+++ app/src/main/java/com/pinkteam/android/healthkon/Controllers/SplashActivity.java
@@ -2,6 +2,7 @@ package com.pinkteam.android.healthkon.Controllers;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.PersistableBundle;
 
 import androidx.annotation.Nullable;
@@ -10,27 +11,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.pinkteam.android.healthkon.R;
 
 public class SplashActivity  extends AppCompatActivity {
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
+
+    /** Called when the activity is first created. */
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
         setContentView(R.layout.splash_layout);
 
-        Thread welcomeThread = new Thread() {
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
-                try {
-                    super.run();
-                    sleep(4000);  //Delay of 10 seconds
-                } catch (Exception e) {
-
-                } finally {
-                    Intent i = new Intent(SplashActivity.this,
-                            MainActivity.class);
-                    startActivity(i);
-                    finish();
-                }
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashActivity.this,MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
             }
-        };
-        welcomeThread.start();
+        }, SPLASH_DISPLAY_LENGTH);
     }
 }
